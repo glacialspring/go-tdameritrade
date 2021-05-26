5f82b2ea49850870e275343130e711f07a1bae45
Richard Foo <foo.richard@gmail.com>
2021-05-26 18:43:47 +0800

    delete print

--- tdameritrade/tdameritrade.go
+++ tdameritrade/tdameritrade.go
@@ -165,7 +165,6 @@ func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Requ
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(buf)
 
 	if body != nil {
 		req.Header.Set("Content-Type", "application/json")
